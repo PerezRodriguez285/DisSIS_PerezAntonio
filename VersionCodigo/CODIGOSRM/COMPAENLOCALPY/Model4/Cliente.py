11a584b6f892e7dc81326d1cf8faa28fdb889da2
JoelRM <luishor8956@gmail.com>
2022-08-25 19:55:03 -0500

    Codigos de DDC

---
+++ VersionCodigo/CODIGOSRM/COMPAENLOCALPY/Model4/Cliente.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Cliente:
+    def __init__(self):
+        self.Nombre = None
+        self.Numero telefonico = None
+
+    def Comprar(self, ):
+        pass
+
+    def Probar zapatos(self, ):
+        pass
+
+    def Eleccion de zapatos(self, ):
+        pass
+
---
+++ VersionCodigo/CODIGOSRM/COMPAENLOCALPY/Model4/Empleado.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Empleado:
+    def __init__(self):
+        self.ID = None
+        self.Nombre = None
+        self.Correo electronico = None
+        self.Numero telefonico = None
+
+    def Atender cliente(self, ):
+        pass
+
+    def Apoyo a cliente(self, ):
+        pass
+
+    def Cobrar(self, ):
+        pass
+
+    def Dar recibo(self, ):
+        pass
+
